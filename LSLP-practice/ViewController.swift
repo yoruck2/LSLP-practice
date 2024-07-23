ade72a1a3d43fea57fea66db3b0bcc0aae22082a
yoruck2 <yoruck2@gmail.com>
2024-07-23 15:42:47 +0900

    Initial Commit

---
+++ LSLP-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSLP-practice
+//
+//  Created by dopamint on 7/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
